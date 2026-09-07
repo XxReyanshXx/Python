649f06e03f6bf42e1514ec6963633d7cd4e9877d
XxReyanshXx <reyansh18pro@gmail.com>
2026-09-07 20:29:56 +0530

    commit

---
+++ Rectangle.py
@@ -0,0 +1,17 @@
+class Rectangle():
+    def __init__(self,leanth,width):
+        self.lenght=  leanth
+        self.width = width
+        
+ 
+    
+    def rectangle_area(self):
+        return self.lenght*self.width
+        
+        
+newRectangle = Rectangle(12,10)  
+print("Area of rectangle:",newRectangle.rectangle_area())
+    
+        
+
+
